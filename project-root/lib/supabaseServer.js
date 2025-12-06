6cd0105dec5be6958b2f18d835f09ba9ba8b3b99
RobertKav <C00282028@setu.ie>
2025-12-06 15:38:57 +0000

    Delete project-root directory

--- project-root/lib/supabaseServer.js
+++
@@ -1,9 +0,0 @@
-// lib/supabaseServer.js
-import { createClient } from '@supabase/supabase-js';
-
-
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const key = process.env.SUPABASE_SERVICE_ROLE_KEY; // server-only
-
-
-export const supabaseServer = createClient(url, key);
